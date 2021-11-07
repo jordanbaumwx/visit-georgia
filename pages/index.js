4e5b15dbc1a06a1cb5882d391113c0ca8413b85a
Jordan Baumgardner <developer@jordanbaumgardner.com>
2021-11-07 16:44:33 -0700

    Continuing troubleshoot of github pages deploy.

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,6 @@ import Hero from "../components/Hero";
 
 import fallDrive from "../public/falldrive.jpg";
 
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -13,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="relative h-screen w-screen">
-        <Image
+        <img
           src={fallDrive}
           alt="A picture of a road through trees with fall colors."
           className="absolute inset-0 w-full h-full object-cover"

0393d473e315517f4358f807f5c170271b21a156
Jordan Baumgardner <developer@jordanbaumgardner.com>
2021-11-07 16:21:13 -0700

    Implemented next/Image and fixed image loading issue.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Head from "next/head";
 import Hero from "../components/Hero";
 
+import fallDrive from "../public/falldrive.jpg";
+
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -9,9 +13,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="relative h-screen w-screen">
-        <img
-          src="/falldrive.jpg"
-          alt="main background image"
+        <Image
+          src={fallDrive}
+          alt="A picture of a road through trees with fall colors."
           className="absolute inset-0 w-full h-full object-cover"
         />
         <Hero />

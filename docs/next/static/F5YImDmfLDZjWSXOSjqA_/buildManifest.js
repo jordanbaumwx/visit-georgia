538b7a44e4f2cd018a91a7d90d088046611368f6
Jordan Baumgardner <developer@jordanbaumgardner.com>
2021-11-07 17:23:22 -0700

    Removed docs folder as it's no longer needed.

--- docs/next/static/F5YImDmfLDZjWSXOSjqA_/buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f21ed29df652692c.js"],"/_error":["static/chunks/pages/error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

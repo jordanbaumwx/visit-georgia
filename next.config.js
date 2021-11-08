d1b8854524e206439c405f7959d80e243588683a
Jordan Baumgardner <developer@jordanbaumgardner.com>
2021-11-07 17:20:30 -0700

    Moved from github pages to Vercel for speed/ease.

--- next.config.js
+++
@@ -1,6 +0,0 @@
-const basePath = process.env.NODE_ENV === "production" ? "/visit-georgia" : "";
-
-module.exports = {
-  basePath,
-  assetPrefix: `${basePath}/`,
-};
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Hero from "../components/Hero";
 
 import fallDrive from "../public/falldrive.jpg";
 
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -11,8 +13,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="relative h-screen w-screen">
-        <img
-          src={"/falldrive.jpg"}
+        <Image
+          src={fallDrive}
           alt="A picture of a road through trees with fall colors."
           className="absolute inset-0 w-full h-full object-cover"
         />
